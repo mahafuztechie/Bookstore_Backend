5bb5f01057b766378016d8dc257c7642c8896ab2
mahafuztechie <mahafuzaadeez5@gmail.com>
2022-05-10 12:21:03 +0530

    [mahafuz] added cart controller delete & update cart api

--- Bookstore/Bookstore/Controllers/CartController.cs
+++ Bookstore/Bookstore/Controllers/CartController.cs
@@ -41,6 +41,49 @@ namespace Bookstore.Controllers
                 return this.BadRequest(new { Success = false, message = ex.Message });
             }
         }
+        [HttpPut("Update/{cart_Id}")]
+        public IActionResult UpdateCart(int cart_Id, CartModel cartModel)
+        {
+            try
+            {
+                int userId = Convert.ToInt32(User.Claims.FirstOrDefault(a => a.Type == "Id").Value);
+                var cart = this.cartBL.UpdateCart(cart_Id, cartModel, userId);
+                if (cart != null)
+                {
+                    return this.Ok(new { Success = true, message = "Cart Updated successfully", Response = cart });
+                }
+                else
+                {
+                    return this.BadRequest(new { Success = false, message = "failed to update cart" });
+                }
+            }
+            catch (Exception ex)
+            {
+                return this.BadRequest(new { Success = false, message = ex.Message });
+            }
+        }
+        //delete
+        [HttpDelete("Delete/{cart_Id}")]
+        public IActionResult DeletCart(int cart_Id)
+        {
+            try
+            {
+                int userId = Convert.ToInt32(User.Claims.FirstOrDefault(a => a.Type == "Id").Value);
+                if (this.cartBL.DeleteCart(cart_Id, userId))
+
+                {
+                    return this.Ok(new { Success = true, message = "Cart Deleted Sucessfully" });
+                }
+                else
+                {
+                    return this.BadRequest(new { Success = false, message = "failed dto delete cart" });
+                }
+            }
+            catch (Exception ex)
+            {
+                return this.BadRequest(new { Success = false, message = ex.Message });
+            }
+        }
         [HttpGet("Get")]
         public IActionResult GetCartDetailsByUserid()
         {
